42929e439d6449b039b2d0bcd89e4b6dfac9c530
luna-mrst <cfr107@i.softbank.jp>
2018-11-29 10:39:41 +0900

    削除ボタン作ったー

--- js/calc.js
+++ js/calc.js
@@ -1,40 +1,27 @@
-document.getElementById('tbl').addEventListener('change', event => {
+const tbl = document.getElementById('tbl');
+tbl.addEventListener('change', event => {
     const esa = parseFloat(event.target.value) * 1000;
-    if(esa < 27000 || esa > 33000) {
+    if (esa < 27000 || esa > 33000) {
         event.target.value = '';
         return;
     }
     const parent = event.target.parentNode.parentNode;
     const base_rise = Math.round((((esa - 27000) / 5) + 100)) / 1000;
 
-    find(parent, 'rise').innerText = base_rise || '';
-    find(parent, 'same30').innerText = 30 - base_rise || '';
-    find(parent, 'diff30').innerText = 30 - Math.ceil(base_rise / 2 * 1000) / 1000 || '';
-    find(parent, 'same33').innerText = 33 - Math.ceil(base_rise / 2 * 1000) / 1000 || '';
-    find(parent, 'diff33').innerText = 33 - Math.ceil(base_rise / 4 * 1000) / 1000 || '';
+    findFirstByClassName(parent, 'rise').innerText = base_rise || '';
+    findFirstByClassName(parent, 'same30').innerText = 30 - base_rise || '';
+    findFirstByClassName(parent, 'diff30').innerText = 30 - Math.ceil(base_rise / 2 * 1000) / 1000 || '';
+    findFirstByClassName(parent, 'same33').innerText = 33 - Math.ceil(base_rise / 2 * 1000) / 1000 || '';
+    findFirstByClassName(parent, 'diff33').innerText = 33 - Math.ceil(base_rise / 4 * 1000) / 1000 || '';
 
+});
+tbl.addEventListener('click', event => {
+    const elm = event.target;
+    if(!elm.classList.contains('deleteButton')) return;
+    elm.parentNode.parentNode.parentNode.removeChild(elm.parentNode.parentNode);
 });
 
 document.getElementById('add').addEventListener('click', event => {
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
-    const inp = document.createElement('input');
-    Object.entries({type: 'number', min: '27.000', max: '33.000', step: '0.001'}).forEach(v => inp.setAttribute(v[0], v[1]));
-    td.appendChild(inp);
-    tr.appendChild(td);
-    ['rise', 'same30', 'diff30', 'same33', 'diff33'].forEach(v => {
-        const td = document.createElement('td');
-        td.classList.add(v);
-        tr.appendChild(td);
-    });
-    document.getElementById('tbl').appendChild(tr);
-    inp.focus();
+    const tr = document.getElementById('tbl').appendChild(createDOM(getTrDefine()));
+    findFirstByClassName(tr, 'inp').focus();
 });
-
-function find(parent, target) {
-    let ret = null;
-    [].forEach.call(parent.children, c => {
-        if(c.classList.contains(target)) ret = c;
-    });
-    return ret;
-}
---
+++ js/define.js
@@ -0,0 +1,76 @@
+function getTrDefine() {
+    return {
+        tag: 'tr',
+        children: [
+            {
+                tag: 'td',
+                children: [{
+                    tag: 'button',
+                    class: ['deleteButton'],
+                    text: '×'
+                }]
+            },
+            {
+                tag: 'td',
+                children: [{
+                    tag: 'input',
+                    class: ['inp'],
+                    attr: {
+                        type: 'number',
+                        min: '27.000',
+                        max: '33.000',
+                        step: '0.001'
+                    }
+                }]
+            },
+            {
+                tag: 'td',
+                class: ['rise']
+            },
+            {
+                tag: 'td',
+                class: ['same30']
+            },
+            {
+                tag: 'td',
+                class: ['diff30']
+            },
+            {
+                tag: 'td',
+                class: ['same33']
+            },
+            {
+                tag: 'td',
+                class: ['diff33']
+            }
+        ]
+    };
+}
+
+// define: {tag:タグ名, class:クラス名配列, attr:属性名キー属性値バリューのオブジェクト, children:子要素のdefine配列, text:テキストノード内容}
+function createDOM(define) {
+    if (define.tag === undefined) return null;
+    const elm = document.createElement(define.tag);
+    if (Array.isArray(define.class))
+        elm.classList.add(...define.class);
+    if (define.attr instanceof Object)
+        Object.entries(define.attr).forEach(v => elm.setAttribute(v[0], v[1]));
+    if (Array.isArray(define.children))
+        define.children.forEach(c => elm.appendChild(createDOM(c)));
+    if (define.text !== undefined)
+        elm.innerText = define.text;
+    return elm;
+}
+
+// element: 探索するDOMelement
+// target: 探索するクラス名
+function findFirstByClassName(element, target) {
+    const queue = [];
+    queue.push(element);
+    while (true) {
+        const elm = queue.shift();
+        if (elm == undefined) return;
+        if (elm.classList.contains(target)) return elm;
+        [].forEach.call(elm.children, c => queue.push(c));
+    }
+}
